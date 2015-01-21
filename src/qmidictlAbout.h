47675dc98f39ff9964e1bd5034251c98da1d2270
rncbc <rncbc@t430.rncbc.lan>
2015-01-21 16:56:44 +0000

    - New year copyright bump.

--- src/qmidictlAbout.h
+++ src/qmidictlAbout.h
@@ -1,7 +1,7 @@
 // qmidictlAbout.h
 //
 /****************************************************************************
-   Copyright (C) 2010-2014, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2010-2015, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -33,7 +33,7 @@
 
 #define QMIDICTL_SUBTITLE     "A MIDI Remote Controller via UDP/IP Multicast"
 #define QMIDICTL_WEBSITE      "http://qmidictl.sourceforge.net"
-#define QMIDICTL_COPYRIGHT    "Copyright (C) 2010-2014, rncbc aka Rui Nuno Capela. All rights reserved."
+#define QMIDICTL_COPYRIGHT    "Copyright (C) 2010-2015, rncbc aka Rui Nuno Capela. All rights reserved."
 
 #define QMIDICTL_DOMAIN       "rncbc.org"
 
